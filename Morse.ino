4970aa61de7d36bbc6f96a21586588deacbd5e3a
Dimitar Bezhanovski <30288047+dimitarbez@users.noreply.github.com>
2020-05-02 22:20:45 +0200

    improved timings

--- Morse.ino
+++ Morse.ino
@@ -13,6 +13,8 @@
 #define LASER 23
 #define dot 200
 #define lon 800
+#define LETTER_DELAY 800
+#define WORD_DELAY 1300
 
 BluetoothSerial SerialBT;
 
@@ -24,144 +26,172 @@ void convertToMorse(char c)
     case 'a':
       blinkOn(dot);
       blinkOn(lon);
-
+      delay(LETTER_DELAY);
       break;
     case 'b':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'c':
       blinkOn(800);
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'd':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'e':
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'f':
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'g':
       blinkOn(lon);
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'h':
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'i':
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
    case 'j':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'k':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'l':
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'm':
       blinkOn(lon);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'n':
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'o':
       blinkOn(lon);
       blinkOn(lon);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'p':
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 'q':
       blinkOn(lon);
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'r':
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 's':
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
       break;
     case 't':
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'u':
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'v':
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'w':
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'x':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'y':
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(lon);
       blinkOn(lon);
+      delay(LETTER_DELAY);
       break;
     case 'z':
       blinkOn(lon);
       blinkOn(lon);
       blinkOn(dot);
       blinkOn(dot);
+      delay(LETTER_DELAY);
+      break;
+    case ' ':
+      delay(WORD_DELAY);
       break;
   }
 }
 
 void blinkOn(int duration)
 {
-  delay(500);
+  delay(300);
   digitalWrite(LASER, HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(duration);
